e97c736486812c74f75b19cbc12e2db2c483d29b
sling254 <mwanikidenis7@gmail.com>
2022-01-26 11:56:03 +0300

    add basic file structure

---
+++ pisr_info/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PisrInfoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'pisr_info'
---
+++ pisr_info/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def IndexView(request):
+    return render(request, 'pisr_info/index.html')
